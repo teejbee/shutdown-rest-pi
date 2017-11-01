374b88824b6ad9fdbf7df2bfc30cc8c4c399ad1b
bjanakiram <Balaji_Janakiram@Dell.com>
2017-11-01 12:02:27 +0530

    bugfix

--- rest_intf.py
+++ rest_intf.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
 from flask import Flask
+from flask import Response
 
 app = Flask(__name__)
 
 @app.route('/restpi/shutdown', methods=['GET'])
 def shutdown():
     print "shutdown called"
+    resp = Response(status = 200)
+    return resp
 
 if __name__ == '__main__':
     app.run(debug=True)
