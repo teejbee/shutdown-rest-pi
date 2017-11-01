148e4641c43573d31e222fafb3646982e47a55a7
tjbalaji@pi <tjbalaji@gmail.com>
2017-11-01 15:21:35 +0530

    added response

--- rest_intf.py
+++ rest_intf.py
@@ -2,14 +2,16 @@
 
 from flask import Flask
 from flask import Response
+from subprocess import call
 
 app = Flask(__name__)
 
 @app.route('/restpi/shutdown', methods=['GET'])
 def shutdown():
     print "shutdown called"
-    resp = Response(status = 200)
+    call("sudo nohup shutdown -h now", shell=True)
+    resp = Response("OK Sir, I will shutdown now!", status = 200)
     return resp
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
