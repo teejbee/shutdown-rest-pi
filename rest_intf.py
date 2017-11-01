27bc178b75a6b6ab7aeb7fd692b0751142966b89
bjanakiram <Balaji_Janakiram@Dell.com>
2017-11-01 15:22:36 +0530

    Added hello method

--- rest_intf.py
+++ rest_intf.py
@@ -5,6 +5,12 @@ from flask import Response
 
 app = Flask(__name__)
 
+@app.route('/restpi/hello', methods=['GET'])
+def ping():
+    print "pi is responding"
+    resp = Response(status = 200)
+    return resp
+
 @app.route('/restpi/shutdown', methods=['GET'])
 def shutdown():
     print "shutdown called"
